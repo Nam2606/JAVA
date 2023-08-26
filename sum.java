372edcc269ea7cbcc3863b1503c5964791324d69
Nam2606 <namratadabhade63@!gnmail.com>
2023-08-26 12:20:40 +0530

    NEW CODES

---
+++ sum.java
@@ -0,0 +1,25 @@
+public class sum {
+public static void main(String[] args) {
+    int a,b,sum,div,mul,sub;
+    a=10;
+    b=20;
+    sum=a+b;
+    System.out.println("sum is\t"+sum);
+    a=4;
+    b=2;
+    div=a/b;
+    System.out.println("div is\t"+div);
+    a=2;
+    b=3;
+    mul=a*b;
+    System.out.println("multiplication is\t"+mul);
+    a=10;
+    b=5;
+    sub=a-b;
+    System.out.println("sub is\t"+sub
+    
+    
+
+    );
+}
+}
